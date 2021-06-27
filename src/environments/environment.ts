6f59a2085966818760252ff03f280474dbcaf302
Luis Alberto Pérez Coello <luisalbertodev@gmail.com>
2021-06-27 13:30:12 -0500

    added config from firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,14 @@
 export const environment = {
   production: false,
   baseUrl: 'http://platzi-store.herokuapp.com',
+  firebase: {
+    apiKey: 'AIzaSyDGY98ykGjfz9Gra-oIAA8iYEkyyULRCYM',
+    authDomain: 'starting-with-angular-v12.firebaseapp.com',
+    projectId: 'starting-with-angular-v12',
+    storageBucket: 'starting-with-angular-v12.appspot.com',
+    messagingSenderId: '937656280504',
+    appId: '1:937656280504:web:45691ec17738c70ab21b4d',
+  },
 };
 
 /*
